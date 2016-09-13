7df3df2ee80e6b9a3ab97280b5858f2ae0387a52
sergiog77 <sergiogomezr77@gmail.com>
2016-09-13 18:01:24 +0200

    proyecto incial

--- CRM_Replicado_Atributos/Form1.Designer.cs
+++ CRM_Replicado_Atributos/Form1.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.Location = new System.Drawing.Point(29, 24);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(222, 514);
+            this.treeView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(273, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 82);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Actualizar Atributos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 550);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- CRM_Replicado_Atributos/Form1.cs
+++ CRM_Replicado_Atributos/Form1.cs
@@ -7,14 +7,119 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Client;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Client.Services;
+using Microsoft.Xrm.Sdk.Metadata;
+using System.ServiceModel.Description;
+using Microsoft.Xrm.Sdk.Client;
+using CRM_Replicado_Atributos.AppCode;
+
+
 
 namespace CRM_Replicado_Atributos
 {
     public partial class Form1 : Form
     {
+        public static OrganizationServiceProxy _serviceProxy;
+        private static OrganizationService _orgService;
+        public static Uri oUri = new Uri("https://legdevcr.legalitas.es/lmsmultasfusdes/XRMServices/2011/Organization.svc");
+        ClientCredentials clientCredentials = new ClientCredentials();
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            clientCredentials.UserName.UserName = "sergio.gomez@legalitas.es";
+            clientCredentials.UserName.Password = "legalitas2";
+
+            CrmHelper crm = new CrmHelper();
+            crm.Conexion = new Uri("https://legdevcr.legalitas.es/lmsmultasfusdes/XRMServices/2011/Organization.svc");
+            crm.credenciales = clientCredentials;
+            _serviceProxy = crm.CrmService();
+
+            RetrieveAllEntitiesRequest metaDataRequest = new RetrieveAllEntitiesRequest();
+            RetrieveAllEntitiesResponse metaDataResponse = new RetrieveAllEntitiesResponse();
+            metaDataRequest.EntityFilters = EntityFilters.Entity;
+            metaDataResponse = (RetrieveAllEntitiesResponse)_serviceProxy.Execute(metaDataRequest);
+            metaDataRequest.EntityFilters = EntityFilters.Entity;
+            metaDataResponse = (RetrieveAllEntitiesResponse)_serviceProxy.Execute(metaDataRequest);
+            //obtenemos el listado de las entiedades de multas
+
+            var entities = metaDataResponse.EntityMetadata.Where(EntityMetadata => EntityMetadata.IsCustomizable.Value == true).OrderBy(EntityMetadata => EntityMetadata.LogicalName);
+
+            foreach (EntityMetadata entity in entities)
+            {
+                TreeNode nodo = new TreeNode();
+                nodo.Text = entity.LogicalName;
+                ObtenerAtributosEntidad(entity, nodo);
+                treeView1.Nodes.Add(nodo);
+                treeView1.EndUpdate();
+            }
+
+
+        }
+
+
+        public static void ObtenerAtributosEntidad(EntityMetadata entity, TreeNode nodo)
+        {
+
+            RetrieveEntityRequest entityRequest1 = new RetrieveEntityRequest
+            {
+                EntityFilters = EntityFilters.Attributes,
+                LogicalName = entity.LogicalName,
+                RetrieveAsIfPublished = true
+            };
+
+            RetrieveEntityResponse entityResponse1 = (RetrieveEntityResponse)_serviceProxy.Execute(entityRequest1);
+
+
+            foreach (AttributeMetadata atributo in entityResponse1.EntityMetadata.Attributes)
+            {
+                TreeNode nodochild = new TreeNode();
+                nodochild.Text = atributo.LogicalName;
+                nodo.Nodes.Add(nodochild);
+
+            }
+
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (TreeNode tn in treeView1.Nodes)
+            {
+                if (tn.Checked)
+                {
+
+                    foreach (TreeNode child in tn.Nodes)
+                    {
+                        if (child.Checked)
+                        { 
+                            string  nombre = child.Text;
+                        }
+
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
     }
 }
